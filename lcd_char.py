ff577b6fa07d6ac5ea8154238ac39cde8eeba7a6
Stephen Angelico <raaff35lightning@gmail.com>
2019-04-20 13:45:42 +1000

    Add and use sub data

--- lcd_char.py
+++ lcd_char.py
@@ -14,5 +14,15 @@ lcd_rows = 2
 lcd_backlight = 4
 lcd = Adafruit_CharLCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
+# Load sub data
+subs = []
+with open('Subs.txt') as f:
+	for line in f:
+		partial_lines = line.split('|')
+		new_partial = []
+		for s in partial_lines:
+			stripped = s.strip()
+			new_partial.append(stripped)
+		subs.append('\n'.join(new_partial))
 
 #if __name__ == '__main__':
