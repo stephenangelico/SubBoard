a52dc42f1ac00b42e5bc63a34d6014c8347a67df
Stephen Angelico <raaff35lightning@gmail.com>
2021-03-06 11:52:20 +1100

    lcd_char: Explicitly disable LCD cursor blink

--- lcd_char.py
+++ lcd_char.py
@@ -13,6 +13,7 @@ lcd_columns = 16
 lcd_rows = 2
 lcd_backlight = 4
 lcd = Adafruit_CharLCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+lcd.blink(False)
 
 # Load sub data
 subs = []
