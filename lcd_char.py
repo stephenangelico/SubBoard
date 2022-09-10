f15ff6c8863c5b72945afcd94a571dcad904f02c
Stephen Angelico <raaff35lightning@gmail.com>
2022-09-10 10:36:07 +1000

    Complete TODOs on software-enabled backlight

--- lcd_char.py
+++ lcd_char.py
@@ -13,9 +13,7 @@ lcd_d6 = digitalio.DigitalInOut(board.D19)
 lcd_d7 = digitalio.DigitalInOut(board.D26)
 lcd_columns = 16
 lcd_rows = 2
-lcd_backlight = digitalio.DigitalInOut(board.D21) # This is useless as backlight is powered from +5V.
-# TODO: Update library to adafruit_character_lcd.character_lcd from adafruit-circuitpython-charlcd
-# TODO: power display from GPIO 21 so display is dark until needed. Requires lcd.backlight = True which is only supported on new library.
+lcd_backlight = digitalio.DigitalInOut(board.D21)
 lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 lcd.backlight = True
 lcd.blink = False
